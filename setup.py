9a413c28779c78c7638c4a3cfe4d3e51b482019e
Domanski <mdomanski@usgs.gov>
2017-11-08 10:19:05 -0600

    Removed pyqt from install_requirements in setup.py

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python 3.6'
     ],
-    python_requires='>=3', requires=['matplotlib', 'pyqt=>5.6', 'numpy', 'pandas', 'statsmodels', 'scipy']
+    python_requires='>=3', 
+	install_requires=['matplotlib', 'numpy', 'pandas', 'statsmodels', 'scipy']
 )
